a38aa09ca29b1a57dc674337058901d450ff8c49
M-Faith060 <159757364+M-Faith060@users.noreply.github.com>
2025-09-15 09:05:25 +0300

    Add files via upload

---
+++ js-project/array.js
@@ -0,0 +1,192 @@
+const staffArray =[
+    {
+        id: 1,
+        name: "Beatrice Njoki Muiruri",
+        title: "admin",
+        
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 2,
+        name: "Rev.Harrison Otieno Randa",
+        title: "BA Coordinator",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 3,
+        name: "Rev. Sebastian Muthoka",
+        title: "welfare coordinator",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 4,
+        name: "Rev. John Mungai",
+        title: "online and distance elearning",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 5,
+        name: "Prof. Dedan Marete",
+        title: "Student dean",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 6,
+        name: "Mary Ireri",
+        title: "Odel",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 7,
+        name: "Patrick Kamau",
+        title: "Welfare",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 8,
+        name: "George Kinyanjui",
+        title: "ICT",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 9,
+        name: "Regina Njoroge",
+        title: "academic dean",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 10,
+        name: "Rev Andrew Manyeki",
+        title: "Deputy Principal",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 11,
+        name: "Prof. Bernard Kariuki",
+        title: "Principal",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 12,
+        name: "Mary Ngugi",
+        title: "Counselling coordinator",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 13,
+        name: "Prof.Billy Opunga",
+        title: "House Master",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 14,
+        name: "Milkah Mungai",
+        title: "Finance",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 15,
+        name: "Sikiyo Sanare",
+        title: "Hospitality",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 16,
+        name: "Stanely Wekesa",
+        title: "Assistance Finance",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 17,
+        name: "Mary Junior",
+        title: "Odel",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+     {
+        id: 18,
+        name: "Timothy Oliwa",
+        title: "Hospitality",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    }, {
+        id: 19,
+        name: "Wilson Wahungi",
+        title: "Landscape",
+        description:"lorem ipsum hgfsdu fjgyhff jfyhjkyhkiuriof jfhiohifyri",
+        socials:{
+            facebook:'https://facebook.com',
+            instagram: 'https://instagram.com'
+        }
+    },
+]
